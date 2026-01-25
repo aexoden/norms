9b9e1819a1d2bab198b61aa19860e67e2ceceb47
Jason Lynch <jason@aexoden.com>
2026-01-25 09:34:08 -0800

    test: add testing infrastructure
    Signed-off-by: Jason Lynch <jason@aexoden.com>

---
+++ tests/test_checks.py
@@ -0,0 +1,10 @@
+# SPDX-License-Identifier: MIT
+# SPDX-FileCopyrightText: 2024 Jason Lynch <jason@aexoden.com>
+"""Check tests for norms."""
+
+from __future__ import annotations
+
+
+def test_placeholder() -> None:
+    """A placeholder test to ensure the tests package is recognized."""
+    assert True
